3552d33f1fe4e0f8df045ef041850d9ec1137806
RubberDuck6919 <joel.hammond122@gmail.com>
2026-02-22 23:10:33 +0000

    Added Plater

---
+++ fuckknows/player.gd
@@ -0,0 +1,33 @@
+extends Area2D
+
+@export var speed = 100
+var screen_size
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size # Replace with function body.
+	hide()
+pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2.ZERO
+	
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y -= 1
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play("Blink")
+	else :
+		$AnimatedSprite2D.stop()
+	
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size) 
+
+pass
