1b9ee6cdd439b7fad0108427bca35773cbbbda5a
RubberDuck6919 <joel.hammond122@gmail.com>
2026-02-23 18:27:18 +0000

    Movement Added

---
+++ fuckknows/character_body_2d.gd
@@ -0,0 +1,29 @@
+extends CharacterBody2D
+
+@export var speed = 200
+var screen_size
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size # Replace with function body.
+pass
+func _process(delta):
+	velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+		
+func _physics_process(delta):
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+pass	
