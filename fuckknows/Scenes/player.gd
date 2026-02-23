8fc350db883d33c983f48ef1cea4260e77ecf095
RubberDuck6919 <joel.hammond122@gmail.com>
2026-02-23 21:18:26 +0000

    Test

--- fuckknows/Scenes/player.gd
+++ fuckknows/Scenes/player.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends Node
 signal hit
 
 # Called when the node enters the scene tree for the first time.
@@ -9,3 +9,8 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_area_2d_hit() -> void:
+	hit.emit()
+	pass # Replace with function body.
