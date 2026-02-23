7b2945ee674976d82474580bb9dd835a584d690f
RubberDuck6919 <joel.hammond122@gmail.com>
2026-02-23 18:15:30 +0000

    s

--- fuckknows/player.gd
+++ fuckknows/player.gd
@@ -1,30 +1,17 @@
 extends Area2D
 
 @export var speed = 3
+@export var velocity = 1
 var screen_size
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size # Replace with function body.
-	hide()
 pass
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	var velocity = Vector2.ZERO
+func get_input():
+	var input_direction = Input.get_vector("move_down","move_left","move_right","move_up")
+	velocity = input_direction * speed
 	
-	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("move_up"):
-		velocity.y += 1
-	if Input.is_action_pressed("move_down"):
-		velocity.y -= 1
-	
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-	
-	position += velocity * delta
-	position = position.clamp(Vector2.ZERO, screen_size) 
-
+func _physics_process(delta):
+	get_input()
+	move_and_slide(velocity)
 pass
